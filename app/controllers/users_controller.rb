b401897f5062988bd2bd15954de700f197f1122a
Peter Zingg <peter@zingg.name>
2008-11-11 14:44:19 -0800

    role_requirement

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   before_filter :require_no_user, :only => [:new, :create]
-  before_filter :require_user, :only => [:show, :edit, :update, :destroy]
+  before_filter :require_user, :only => [:index, :show, :edit, :update, :destroy]
+  require_role "admin", :for => [:index, :destroy]
   
   # GET /users
   # GET /users.xml
