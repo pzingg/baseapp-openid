844ce027e96521c3bd8a21d4a2cc5149f115296a
Peter Zingg <peter@zingg.name>
2008-11-11 12:34:07 -0800

    rails generated users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
+  map.resources :users
+
   # The priority is based upon order of creation: first created -> highest priority.
 
   # Sample of regular route:
@@ -32,7 +34,15 @@ ActionController::Routing::Routes.draw do |map|
 
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
   # map.root :controller => "welcome"
-
+  
+  # authlogic authentication
+  map.resource :user_session
+  map.default "/", :controller => "user_sessions", :action => "new"
+  
+  # user accounts
+  map.resource :account, :controller => "users"
+  map.resources :users
+  
   # See how all your routes lay out with "rake routes"
 
   # Install the default routes as the lowest priority.
